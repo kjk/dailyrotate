401222aef1dd647284725cced8b9552df1402d7d
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2017-05-09 18:59:17 -0700

    add Write2() that also return file and offset where we wrote the data

--- daily_rotate_file.go
+++ daily_rotate_file.go
@@ -1,6 +1,7 @@
 package dailyrotate
 
 import (
+	"io"
 	"os"
 	"path/filepath"
 	"sync"
@@ -17,6 +18,9 @@ type File struct {
 	path     string
 	file     *os.File
 	onRotate func(path string, didRotate bool)
+
+	// for tests only
+	lastWriteCurrPos int64
 }
 
 func (f *File) close(didRotate bool) error {
@@ -28,6 +32,7 @@ func (f *File) close(didRotate bool) error {
 	if f.onRotate != nil && err != nil {
 		f.onRotate(f.path, didRotate)
 	}
+	f.day = 0
 	return err
 }
 
@@ -43,8 +48,13 @@ func (f *File) open() error {
 		return err
 	}
 
-	flag := os.O_CREATE | os.O_APPEND | os.O_WRONLY
+	// would be easier to open with os.O_APPEND but Seek() doesn't work in that case
+	flag := os.O_CREATE | os.O_WRONLY
 	f.file, err = os.OpenFile(f.path, flag, 0644)
+	if err != nil {
+		return err
+	}
+	_, err = f.file.Seek(0, io.SeekEnd)
 	return err
 }
 
@@ -62,15 +72,11 @@ func (f *File) reopenIfNeeded() error {
 }
 
 // NewFile opens a new log file (creates if doesn't exist, will append if exists)
-func NewFile(pathFormat string, onRotate func(path string, didRotate bool)) (*File, error) {
-	res := &File{
+func NewFile(pathFormat string, onRotate func(path string, didRotate bool)) *File {
+	return &File{
 		pathFormat: pathFormat,
 		onRotate:   onRotate,
 	}
-	if err := res.open(); err != nil {
-		return nil, err
-	}
-	return res, nil
 }
 
 // Close closes the file
@@ -80,15 +86,41 @@ func (f *File) Close() error {
 	return f.close(false)
 }
 
+func (f *File) write(d []byte, flush bool) (int64, int, error) {
+	err := f.reopenIfNeeded()
+	if err != nil {
+		return 0, 0, err
+	}
+	f.lastWriteCurrPos, err = f.file.Seek(0, io.SeekCurrent)
+	if err != nil {
+		return 0, 0, err
+	}
+	n, err := f.file.Write(d)
+	if err != nil {
+		return 0, n, err
+	}
+	if flush {
+		err = f.file.Sync()
+	}
+	return f.lastWriteCurrPos, n, err
+}
+
 // Write writes data to a file
 func (f *File) Write(d []byte) (int, error) {
 	f.Lock()
 	defer f.Unlock()
-	err := f.reopenIfNeeded()
-	if err != nil {
-		return 0, err
-	}
-	return f.file.Write(d)
+	_, n, err := f.write(d, false)
+	return n, err
+}
+
+// Write2 writes data to a file, optionally flushes. To enable users to later
+// seek to where the data was written, it returns name of the file where data
+// was written, offset at which the data was written, number of bytes and error
+func (f *File) Write2(d []byte, flush bool) (string, int64, int, error) {
+	f.Lock()
+	defer f.Unlock()
+	currPos, n, err := f.write(d, flush)
+	return f.path, currPos, n, err
 }
 
 // Flush flushes the file
--- daily_rotate_file_test.go
+++ daily_rotate_file_test.go
@@ -16,20 +16,38 @@ func TestBasic(t *testing.T) {
 	assert.Nil(t, err)
 	pathFormat := filepath.Join("test_dir", "second", "2006-01-02.txt")
 	pathExp := time.Now().UTC().Format(pathFormat)
-	f, err := NewFile(pathFormat, nil)
-	assert.Nil(t, err)
+	f := NewFile(pathFormat, nil)
 	assert.NotNil(t, f)
-	assert.Equal(t, pathExp, f.path)
 	n, err := io.WriteString(f, "hello\n")
 	assert.Nil(t, err)
+	assert.Equal(t, 0, int(f.lastWriteCurrPos))
 	assert.Equal(t, n, 6)
+	assert.Equal(t, pathExp, f.path)
 	n, err = f.Write([]byte("bar\n"))
 	assert.Nil(t, err)
+	assert.Equal(t, 6, int(f.lastWriteCurrPos))
 	assert.Equal(t, n, 4)
-	f.Close()
+	err = f.Close()
+	assert.Nil(t, err)
+
 	d, err := ioutil.ReadFile(pathExp)
 	assert.Nil(t, err)
 	assert.Equal(t, string(d), "hello\nbar\n")
+
+	path, off, n, err := f.Write2([]byte("and more\n"), true)
+	assert.Nil(t, err)
+
+	assert.Equal(t, len(d), int(off))
+
+	assert.Equal(t, 9, n)
+	assert.Equal(t, pathExp, path)
+
+	err = f.Close()
+	assert.Nil(t, err)
+	d, err = ioutil.ReadFile(pathExp)
+	assert.Nil(t, err)
+	assert.Equal(t, string(d), "hello\nbar\nand more\n")
+
 	err = os.RemoveAll("test_dir")
 	assert.Nil(t, err)
 }
